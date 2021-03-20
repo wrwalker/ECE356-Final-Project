49b4056681c8e9f35475d43b820e3b34b8bac95a
William Walker <wrwalker427@gmail.com>
2021-03-20 19:41:16 -0400

    added 2 columns trumpOrBiden and sentimentScore. also made every attribute follow camelcase instead of LikeThis

--- Database Loading Scripts/load-data.sql
+++ Database Loading Scripts/load-data.sql
@@ -14,16 +14,18 @@ select 'Create ElectionTweets' as '';
 --                             votes, 
 --                             won
 
-create table ElectionTweets (TweetID BIGINT not null,
-							Tweet varchar(1960) not null,
-                            Likes int,
-                            RetweetCount int,
-                            UserID BIGINT not null,
-                            UserFollowersCount int,
-                            City varchar(255),
-                            Country char(30) not null,
-                            StateCode char(4) not null,
-                            primary key (TweetID));
+create table ElectionTweets (tweetID BIGINT not null,
+							tweet varchar(1960) not null,
+                            likes int,
+                            retweetCount int,
+                            userID BIGINT not null,
+                            userFollowersCount int,
+                            city varchar(255),
+                            country char(30) not null,
+                            stateCode char(4) not null,
+                            trumpOrBiden char(1) not null,
+                            sentimentScore decimal(5, 2),
+                            primary key (tweetID));
 
 load data infile '/var/lib/mysql-files/ece356/election-tweets/hashtag_donaldtrump.csv' ignore into table ElectionTweets
      fields terminated by ','
@@ -31,23 +33,54 @@ load data infile '/var/lib/mysql-files/ece356/election-tweets/hashtag_donaldtrum
      lines terminated by '\n'
      ignore 1 lines(
      @throwAway,
-     TweetID,
-     Tweet,
-     Likes,
-     RetweetCount,
+     tweetID,
+     tweet,
+     likes,
+     retweetCount,
      @throwAway,
-     UserID,
-     @throwAway,
-     @throwAway,
-     @throwAway,
-     @throwAway,
-     UserFollowersCount,
+     userID,
      @throwAway,
      @throwAway,
      @throwAway,
-     City,
-     Country,
+     @throwAway,
+     userFollowersCount,
      @throwAway,
      @throwAway,
-     StateCode,
-     @throwAway);
+     @throwAway,
+     city,
+     country,
+     @throwAway,
+     @throwAway,
+     stateCode,
+     @throwAway,
+     @TrumpOrBiden)
+     set trumpOrBiden = 'T';
+     
+load data infile '/var/lib/mysql-files/ece356/election-tweets/hashtag_joebiden.csv' ignore into table ElectionTweets
+     fields terminated by ','
+     enclosed by '"'
+     lines terminated by '\n'
+     ignore 1 lines(
+     @throwAway,
+     tweetID,
+     tweet,
+     likes,
+     retweetCount,
+     @throwAway,
+     userID,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     userFollowersCount,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     city,
+     country,
+     @throwAway,
+     @throwAway,
+     stateCode,
+     @throwAway,
+     @TrumpOrBiden)
+     set trumpOrBiden = 'B';
