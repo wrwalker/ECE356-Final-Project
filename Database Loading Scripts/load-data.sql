8cf9ba47ed7db6ba45d69a388a8c00033f251525
William Walker <wrwalker427@gmail.com>
2021-03-20 18:41:08 -0400

    Got the proper script this time

--- Database Loading Scripts/load-data.sql
+++ Database Loading Scripts/load-data.sql
@@ -15,41 +15,39 @@ select 'Create ElectionTweets' as '';
 --                             won
 
 create table ElectionTweets (TweetID BIGINT not null,
-							Tweet varchar(300) not null,
+							Tweet varchar(1960) not null,
                             Likes int,
                             RetweetCount int,
                             UserID BIGINT not null,
                             UserFollowersCount int,
-                            City char(20),
-                            Country char(20) not null,
+                            City varchar(255),
+                            Country char(30) not null,
                             StateCode char(4) not null,
                             primary key (TweetID));
-                            
-
--- gameID decimal(10),
---        	     	   season decimal(8) not null,
--- 				   type char(1) not null,
--- 				   dateTimeGMT datetime not null,
--- 				   awayTeamID int not null,
--- 				   homeTeamID int not null,
--- 				   awayGoals int not null check(awayGoals >= 0),
--- 				   homeGoals int not null check(homeGoals >= 0),
--- 				   outcome char(12) not null,
--- 				   homeRinkSideStart char(50) not null,
--- 				   venue varchar(64) not null,
--- 				   venueTimeZoneID char(20) not null,
--- 				   venueTimeZoneOffset char(50) not null,
--- 				   venueTimeZoneTZ char(3) not null,
--- -- Key Constraints
--- 				   primary key (gameID),
--- 				   foreign key (awayTeamID) references TeamInfo(teamID),
--- 				   foreign key (homeTeamID) references TeamInfo(teamID)
--- -- Integrity Constraints
--- -- Set above showing inline format
--- 		  );
 
 load data infile '/var/lib/mysql-files/ece356/election-tweets/hashtag_donaldtrump.csv' ignore into table ElectionTweets
      fields terminated by ','
      enclosed by '"'
      lines terminated by '\n'
-     ignore 1 lines;
+     ignore 1 lines(
+     @throwAway,
+     TweetID,
+     Tweet,
+     Likes,
+     RetweetCount,
+     @throwAway,
+     UserID,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     UserFollowersCount,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     City,
+     Country,
+     @throwAway,
+     @throwAway,
+     StateCode,
+     @throwAway);
