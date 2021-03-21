7355c72dd613f47d3ee561e29f1a02fe801e681e
William Walker <wrwalker427@gmail.com>
2021-03-21 14:07:09 -0400

    Moved some scripts

---
+++ Database Loading Scripts/load-tweets.sql
@@ -0,0 +1,76 @@
+use Election;
+drop table if exists ElectionTweets;
+-- ElectionTweets ------------------------------------------------------------------------
+select '-----------------------------------------------------------------' as '';
+select 'Create ElectionTweets' as '';
+
+create table ElectionTweets (tweetID BIGINT not null,
+							tweet varchar(1960) not null,
+                            likes int,
+                            retweetCount int,
+                            userID BIGINT not null,
+                            userFollowersCount int,
+                            city varchar(255),
+                            country char(30) not null CHECK (country <> ''),
+                            stateCode char(4) not null CHECK (stateCode <> ''),
+                            trumpOrBiden char(1) not null,
+                            sentimentScore decimal(5, 2),
+                            primary key (tweetID));
+
+load data infile '/var/lib/mysql-files/ece356/election-tweets/hashtag_donaldtrump.csv' ignore into table ElectionTweets
+     fields terminated by ','
+     enclosed by '"'
+     lines terminated by '\n'
+     ignore 1 lines(
+     @throwAway,
+     tweetID,
+     tweet,
+     likes,
+     retweetCount,
+     @throwAway,
+     userID,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     userFollowersCount,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     city,
+     country,
+     @throwAway,
+     @throwAway,
+     stateCode,
+     @throwAway,
+     @TrumpOrBiden)
+     set trumpOrBiden = 'T';
+     
+load data infile '/var/lib/mysql-files/ece356/election-tweets/hashtag_joebiden.csv' ignore into table ElectionTweets
+     fields terminated by ','
+     enclosed by '"'
+     lines terminated by '\n'
+     ignore 1 lines(
+     @throwAway,
+     tweetID,
+     tweet,
+     likes,
+     retweetCount,
+     @throwAway,
+     userID,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     userFollowersCount,
+     @throwAway,
+     @throwAway,
+     @throwAway,
+     city,
+     country,
+     @throwAway,
+     @throwAway,
+     stateCode,
+     @throwAway,
+     @TrumpOrBiden)
+     set trumpOrBiden = 'B';
