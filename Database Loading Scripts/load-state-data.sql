4d8468c7ec000b44b4ea2307154ebd3010dc941a
William Walker <wrwalker427@gmail.com>
2021-03-21 16:00:52 -0400

    fixed primary keys

--- Database Loading Scripts/load-state-data.sql
+++ Database Loading Scripts/load-state-data.sql
@@ -6,7 +6,7 @@ select 'Create VotesByState' as '';
 create table VotesByState (state char(20) not null,
                             totalVotes int unsigned not null,
                             level char,
-                            primary key (state));
+                            primary key (state, level));
 
 -- Governor by State ------------------
 load data infile '/var/lib/mysql-files/ece356/us-election-2020/governors_state.csv' ignore into table VotesByState
