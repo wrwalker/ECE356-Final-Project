21e55a7e75edbb347ce2fde2980989e57e26c807
William Walker <wrwalker427@gmail.com>
2021-04-15 17:21:11 -0400

    Fixed running with no parameters not displaying help menu

--- SQLClient/src/cmd/root.go
+++ SQLClient/src/cmd/root.go
@@ -14,14 +14,6 @@ var Verbose bool
 var RootCMD = &cobra.Command{
 	Use:   rootStr,
 	Short: fmt.Sprintf("%s is a command line interface (CLI) to interact with the Elections database", rootStr),
-	Run: func(cmd *cobra.Command, args []string) {
-		//qm := queryMaker.NewQueryMaker()
-		//defer qm.Db.Close()
-		//
-		//res, _ := qm.doQuery("SELECT * FROM VotesByCountyCandidate")
-		//rows, colNames, _ := utils.DeserializeRowsToMappedInterface(res)
-		//utils.PrintMap(rows, colNames)
-	},
 }
 
 func Execute() {
