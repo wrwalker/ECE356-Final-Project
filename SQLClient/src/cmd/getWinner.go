5cdb61a90913181f63812973d488538ac8b5b5c0
William Walker <wrwalker427@gmail.com>
2021-04-16 00:15:20 -0400

    actually pushed my sins this time

--- SQLClient/src/cmd/getWinner.go
+++ SQLClient/src/cmd/getWinner.go
@@ -6,45 +6,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const getVotesForCandidate = "GetVotesForCandidate"
+const getWinner = "GetWinner"
 
-var candidateName string
-var states []string
-var county string
-var annotationsBoolean bool
+var stateWhoWon string
+var countyWhoWon string
 
-var getVotesForCandidateCmd = &cobra.Command{
-	Use:   getVotesForCandidate,
-	Short: "Get the total votes for a candidate",
+var getWinnerCmd = &cobra.Command{
+	Use:   getWinner,
+	Short: "Get who won for a given county, state, or all of the USA",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		qm := queryMaker.NewQueryMaker()
 		defer qm.Db.Close()
 
-		numVotes, attemptedQuery, err := qm.GetVotesForCandidate(candidateName, county, states, annotationsBoolean)
+		candidateWhoWon, attemptedQuery, err := qm.GetWinner(stateWhoWon, countyWhoWon, qm)
 		if Verbose {
 			fmt.Printf("Ran: %s\n", attemptedQuery)
 		}
 		if err != nil {
 			return err
 		}
-		fmt.Printf("%s recieved %d votes\n", candidateName, numVotes)
+		fmt.Printf("%s won\n", candidateWhoWon)
 		return nil
 	},
 }
 
 func init() {
-	candidateNameFlag := "candidateName"
-	getVotesForCandidateCmd.Flags().StringVarP(&candidateName, candidateNameFlag, "n", "", "name of candidate to tally Votes")
-	getVotesForCandidateCmd.MarkFlagRequired(candidateNameFlag)
 
 	stateFlag := "state"
-	getVotesForCandidateCmd.Flags().StringSliceVarP(&states, stateFlag, "s", []string{}, "states to tally Votes")
+	getWinnerCmd.Flags().StringVarP(&stateWhoWon, stateFlag, "s", "", "state to see who won")
 
 	countyFlag := "county"
-	getVotesForCandidateCmd.Flags().StringVarP(&county, countyFlag, "c", "", "county to tally Votes")
+	getWinnerCmd.Flags().StringVarP(&countyWhoWon, countyFlag, "c", "", "county to see who won")
 
-	annotationsFlag := "annotations"
-	getVotesForCandidateCmd.Flags().BoolVarP(&annotationsBoolean, annotationsFlag, "a", false, "add this to filter results by counties you've annotated")
-
-	RootCMD.AddCommand(getVotesForCandidateCmd)
+	RootCMD.AddCommand(getWinnerCmd)
 }
